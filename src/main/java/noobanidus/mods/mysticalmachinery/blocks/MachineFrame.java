f392a46cc109ef867bc42be920c97361dc11633a
Jon McManus <due@wxwhatever.com>
2020-01-13 19:21:26 +1000

    Cut down on the number of machine frames available.

--- src/main/java/noobanidus/mods/mysticalmachinery/blocks/MachineFrame.java
+++ src/main/java/noobanidus/mods/mysticalmachinery/blocks/MachineFrame.java
@@ -12,27 +12,27 @@ import noobanidus.mods.mysticalmachinery.MysticalMachinery;
 import javax.annotation.Nullable;
 
 public enum MachineFrame {
-  AMETHYST("amethyst", MWTags.Items.AMETHYST_GEM),
+/*  AMETHYST("amethyst", MWTags.Items.AMETHYST_GEM),*/
   CHORUS("chorus", MWTags.Items.PURPUR),
-  COPPER("copper", MWTags.Items.COPPER_INGOT),
+/*  COPPER("copper", MWTags.Items.COPPER_INGOT),
   DIAMOND("diamond", Tags.Items.GEMS_DIAMOND),
   EMERALD("emerald", Tags.Items.GEMS_EMERALD),
   GOLD("gold", Tags.Items.INGOTS_GOLD),
-  IRON("iron", Tags.Items.INGOTS_IRON),
-  LEAD("lead", MWTags.Items.LEAD_INGOT),
-  NETHER("nether", MWTags.Items.NETHER_BRICKS),
-  PRISMARINE("prismarine", Tags.Items.GEMS_PRISMARINE),
-  QUICKSILVER("quicksilver", MWTags.Items.QUICKSILVER_INGOT),
-  RED_NETHER("red_nether", MWTags.Items.RED_NETHER_BRICKS),
-  SILVER("silver", MWTags.Items.SILVER_INGOT),
-  TERRACOTTA("terracotta", MWTags.Items.TERRACOTTA),
-  TIN("tin", MWTags.Items.TIN_INGOT),
-  WOOD("wood", ItemTags.PLANKS),
+  IRON("iron", Tags.Items.INGOTS_IRON),*/
   LAPIS("lapis", Tags.Items.GEMS_LAPIS),
-  REDSTONE("redstone", Tags.Items.DUSTS_REDSTONE),
-  STONE("stone", Tags.Items.STONE),
+/*  LEAD("lead", MWTags.Items.LEAD_INGOT),*/
+  NETHER("nether", MWTags.Items.NETHER_BRICKS),
+/*  PRISMARINE("prismarine", Tags.Items.GEMS_PRISMARINE),*/
+  QUICKSILVER("quicksilver", MWTags.Items.QUICKSILVER_INGOT),
+/*  REDSTONE("redstone", Tags.Items.DUSTS_REDSTONE),*/
+  RED_NETHER("red_nether", MWTags.Items.RED_NETHER_BRICKS),
   REINFORCED("reinforced", Tags.Items.STORAGE_BLOCKS_IRON),
-  SLIME("slime", MWTags.Items.SLIME)
+/*  SILVER("silver", MWTags.Items.SILVER_INGOT),*/
+  SLIME("slime", MWTags.Items.SLIME),
+  STONE("stone", Tags.Items.STONE),
+  TERRACOTTA("terracotta", MWTags.Items.TERRACOTTA),
+/*  TIN("tin", MWTags.Items.TIN_INGOT),*/
+  WOOD("wood", ItemTags.PLANKS)
   ;
 
   private String name;
